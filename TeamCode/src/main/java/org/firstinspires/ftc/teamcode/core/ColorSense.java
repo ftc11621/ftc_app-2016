1728d50a300db1f8867d5c335a3e99879807ccc0
ftc11621 <suriono@gmail.com>
2016-12-06 17:10:12 -0600

    Debug color sensor class.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/ColorSense.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/ColorSense.java
@@ -1,5 +1,9 @@
 package org.firstinspires.ftc.teamcode.core;
 
+import android.app.Activity;
+import android.graphics.Color;
+import android.view.View;
+
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
@@ -9,27 +13,39 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class ColorSense {
     ColorSensor colorSensor;
+    float hsvValues[] = {0F,0F,0F};
+    final float values[] = hsvValues; // values is a reference to the hsvValues array.
+    View relativeLayout;
 
     public ColorSense(HardwareMap hardwareMap) {
         colorSensor = hardwareMap.colorSensor.get("color_sensor");
-        float hsvValues[] = {0F,0F,0F};
-        final float values[] = hsvValues; // values is a reference to the hsvValues array.
+        //float hsvValues[] = {0F,0F,0F};
+
+        View relativeLayout = ((Activity) hardwareMap.appContext).findViewById(com.qualcomm.ftcrobotcontroller.R.id.RelativeLayout);
 
         colorSensor.enableLed(false);
-
     }
 
-    public static enum Color {red, blue, neither};
+    public static enum Beacon_colors {red, blue, neither};
 
 
-    public Color senseColor() {
-        if (colorSensor.red() > 200) {
-            return Color.red;
-        } else if (colorSensor.blue() > 200) {
-            return Color.blue;
+    public Beacon_colors senseColor() {
+        Color.RGBToHSV(colorSensor.red() * 8, colorSensor.green() * 8, colorSensor.blue() * 8, hsvValues); /// reading color
+        if (colorSensor.red() > 1) {
+            return Beacon_colors.red;
+        } else if (colorSensor.blue() > 1) {
+            return Beacon_colors.blue;
         } else {
-            return Color.neither;
+            return Beacon_colors.neither;
         }
+        // change the background color to match the color detected by the RGB sensor.
+        // pass a reference to the hue, saturation, and value array as an argument
+        // to the HSVToColor method.
+        //relativeLayout.post(new Runnable() {
+        //    public void run() {
+        //        relativeLayout.setBackgroundColor(Color.HSVToColor(0xff, values));
+        //    }
+        //});
     }
 }
 
